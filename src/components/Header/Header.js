f333ad66150f6206bf70d7cc14113cbf3ae2fe85
Megha0109 <42582776+Megha0109@users.noreply.github.com>
2022-09-15 00:26:23 +0530

    Add files via upload

---
+++ src/components/Header/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import resumeSvg from '././assets/resume.svg';
+import Nav1 from '../navigation/Nav';
+import resumebackground from '././assets/resuembackground.png';
+
+import styles from "./Header.module.css";
+function Header() {
+  return (
+    <div className="Nav">
+      <Nav1 />
+
+      <div className={styles.container}>
+        <article
+          className={styles.article}
+          style={{
+            backgroundImage: `url(${resumebackground})`,
+          }}
+        >
+          <div className={styles.left}>
+            <p className={styles.heading}>
+              A <span>Resume</span> that stand out!
+            </p>
+            <p className={styles.heading}>
+              {" "}
+              Make you own Resume.
+              <span>
+                <br></br>
+                It's Free!!
+              </span>
+            </p>
+          </div>
+          <div>
+            <img src={resumeSvg} alt="Resume" height="300px" width="500px" />
+          </div>
+        </article>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/navigation/Nav.js
@@ -0,0 +1,29 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Publify from './Publify.png';
+
+function Nav1() {
+  return (
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">
+          <img
+          src = {Publify}
+           alt='Publify' 
+           height = "50px" 
+           width = "50px"
+           />
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#contactUs">Contact Us</Nav.Link>
+            <Nav.Link href="#aboutUs">About Us</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+  );
+}
+
+export default Nav1;
